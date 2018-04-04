d3ee69f39a31fd3fe532f92453ca6399eaa4fe7b
boveloco <boveloco@gmail.com>
2018-04-04 00:48:45 -0300

    warrior fix

--- initial-items/warrior.sql
+++ initial-items/warrior.sql
@@ -9,12 +9,12 @@ insert into playercreateinfo_item (race, class, itemid, amount) values (7, 1, 42
 insert into playercreateinfo_item (race, class, itemid, amount) values (8, 1, 42945, 1);
 insert into playercreateinfo_item (race, class, itemid, amount) values (11, 1, 42945, 1);
 
-mysql> insert into playercreateinfo_item (race, class, itemid, amount) values (1, 1, 42943, 1);
-mysql> insert into playercreateinfo_item (race, class, itemid, amount) values (2, 1, 42943, 1);
-mysql> insert into playercreateinfo_item (race, class, itemid, amount) values (3, 1, 42943, 1);
-mysql> insert into playercreateinfo_item (race, class, itemid, amount) values (4, 1, 42943, 1);
-mysql> insert into playercreateinfo_item (race, class, itemid, amount) values (5, 1, 42943, 1);
-mysql> insert into playercreateinfo_item (race, class, itemid, amount) values (6, 1, 42943, 1);
-mysql> insert into playercreateinfo_item (race, class, itemid, amount) values (7, 1, 42943, 1);
-mysql> insert into playercreateinfo_item (race, class, itemid, amount) values (8, 1, 42943, 1);
-mysql> insert into playercreateinfo_item (race, class, itemid, amount) values (11, 1, 42943, 1);
+insert into playercreateinfo_item (race, class, itemid, amount) values (1, 1, 42943, 1);
+insert into playercreateinfo_item (race, class, itemid, amount) values (2, 1, 42943, 1);
+insert into playercreateinfo_item (race, class, itemid, amount) values (3, 1, 42943, 1);
+insert into playercreateinfo_item (race, class, itemid, amount) values (4, 1, 42943, 1);
+insert into playercreateinfo_item (race, class, itemid, amount) values (5, 1, 42943, 1);
+insert into playercreateinfo_item (race, class, itemid, amount) values (6, 1, 42943, 1);
+insert into playercreateinfo_item (race, class, itemid, amount) values (7, 1, 42943, 1);
+insert into playercreateinfo_item (race, class, itemid, amount) values (8, 1, 42943, 1);
+insert into playercreateinfo_item (race, class, itemid, amount) values (11, 1, 42943, 1);
